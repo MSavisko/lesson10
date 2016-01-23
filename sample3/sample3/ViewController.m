3cec26efe80465a229407e134463d31a57469d98
Maksym Savisko <stowyn@gmail.com>
2016-01-23 16:58:52 +0200

    Taks #1. Limit of view fix

--- sample3/sample3/ViewController.m
+++ sample3/sample3/ViewController.m
@@ -13,6 +13,8 @@
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *xconstraint;
 @property (weak, nonatomic) IBOutlet UIButton *button;
 @property (nonatomic) NSInteger tapCount;
+@property (nonatomic) CGFloat rightLimitOfScreen;
+@property (nonatomic) CGFloat leftLimitOfScreen;
 
 @end
 
@@ -30,23 +32,21 @@
 
 - (void)viewDidAppear:(BOOL)animated {
 	[super viewDidAppear:animated];
+
+    self.rightLimitOfScreen = self.view.frame.size.width/2 - self.button.frame.size.width/2;
+    self.leftLimitOfScreen = 0 - self.view.frame.size.width/2 +self.button.frame.size.width/2;
     self.tapCount = 0;
-    
-	for (NSLayoutConstraint *constraint in self.view.constraints) {
-		if (constraint.firstAttribute == NSLayoutAttributeCenterX) {
-			constraint.constant = 100.0f;
-			break;
-		}
-	}
-    [self movementAnimation];
+    [self runButtonAnimationWith:4.0f];
 	
 }
 
-- (void) movementAnimation {
-    if (self.xconstraint.constant == 100.f) {
-        self.xconstraint.constant = -100.f;
-    } else self.xconstraint.constant = 100.f;
-    [UIView animateWithDuration:4.4
+- (void) runButtonAnimationWith:(CGFloat)duration {
+    if (self.xconstraint.constant == self.leftLimitOfScreen) {
+        self.xconstraint.constant = self.rightLimitOfScreen;
+    }
+    else self.xconstraint.constant = self.leftLimitOfScreen;
+    
+    [UIView animateWithDuration:duration
                           delay:0.0f
                         options:UIViewAnimationOptionAllowUserInteraction
                      animations:^(void) {
@@ -55,7 +55,7 @@
                      completion:^(BOOL finished) {
                          [self buttonRotation];
                          dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-                             [self movementAnimation];
+                             [self runButtonAnimationWith:7.0f];
                          });
                      }];
 }
