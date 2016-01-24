e5657edbd682045839d4a9f88d0566b160791041
Maksym Savisko <stowyn@gmail.com>
2016-01-24 18:08:49 +0200

    Task #2. Init

---
+++ lesson10. Task#2/lesson10. Task#2/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  lesson10. Task#2
+//
+//  Created by Maksym Savisko on 1/24/16.
+//  Copyright © 2016 Maksym Savisko. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+{
+    UIDynamicAnimator* _animator;
+    UIGravityBehavior* _gravity;
+    UICollisionBehavior* _collision;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
