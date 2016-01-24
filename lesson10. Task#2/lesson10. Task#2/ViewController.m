46c99cba586a8435acc7ebd1336d768982313289
Maksym Savisko <stowyn@gmail.com>
2016-01-24 20:44:40 +0200

    Task #2. Added square by touches

--- lesson10. Task#2/lesson10. Task#2/ViewController.m
+++ lesson10. Task#2/lesson10. Task#2/ViewController.m
@@ -22,11 +22,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    _animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
+    _gravity = [[UIGravityBehavior alloc] init];
+    [_animator addBehavior:_gravity];
+    _collision = [[UICollisionBehavior alloc] init];
+    _collision.translatesReferenceBoundsIntoBoundary = YES;
+    [_animator addBehavior:_collision];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void) touchesBegan:(NSSet*) touches withEvent:(UIEvent *) event {
+    CGPoint point = [[touches anyObject] locationInView:self.view];
+    
+    UIView * square = [[UIView alloc] initWithFrame:CGRectMake(point.x, point.y, 50, 50)];
+    square.backgroundColor = [UIColor grayColor];
+    
+    [self.view addSubview:square];
+    [_gravity addItem:square];
+    [_collision addItem:square];
 }
 
 @end
